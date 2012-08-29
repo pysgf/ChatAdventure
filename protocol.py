dcd42f9ee60c4d6717939508f5c792616429abba
Matthew Thompson <chameleonator@gmail.com>
2012-08-28 21:16:27 -0500

    Adding symbol support

--- protocol.py
+++ protocol.py
@@ -3,11 +3,11 @@
 import re
 
 HELLO = "HELLO {name} {key}\n"
-HELLO_RE = re.compile("^HELLO (?P<name>\w+) (?P<key>\w+)\n$")
+HELLO_RE = re.compile(r"^HELLO (?P<name>\w+) (?P<key>\w+)\n$")
 
 BYE = "BYE {key}\n"
-BYE_RE = re.compile("^BYE (?P<key>\w+)\n$")
+BYE_RE = re.compile(r"^BYE (?P<key>\w+)\n$")
 
 MESSAGE = "SAY {key} {message}\n"
-MESSAGE_RE = re.compile("^SAY (?P<key>\w+) (?P<message>[\w ]+)\n$")
+MESSAGE_RE = re.compile(r"^SAY (?P<key>\w+) (?P<message>[\w`!@#$%^&*()_+-=[\]\\{}|;':\",./<>\? ]+)\n$")
 
